6293806b3cd2c0df300071de3a0bc4e9e8c0f3f6
justinhwon <justinwon94555@gmail.com>
2020-05-28 04:27:43 -0700

    basic frontend formatting (navbar + char cards)

--- SSBU_Stage_Guide/settings.py
+++ SSBU_Stage_Guide/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = secret_key_production
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '192.168.0.152']
 
 
 # Application definition
