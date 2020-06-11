93c6baaead6218fca84c9632ea21df4323adfbb5
justinhwon <justinwon94555@gmail.com>
2020-06-11 00:12:27 -0700

    Working voting form

--- guide/views.py
+++ guide/views.py
@@ -136,12 +136,14 @@ class VoteForm(forms.Form):
     unova = forms.ChoiceField(label='Unova Pokémon League', choices=VOTEVALUES, widget=forms.RadioSelect, required=False)
 
 class VoteModelForm(forms.ModelForm):
-    VOTEVALUES=[(-2,'-2: Awful'),
-         (-1,'-1: Bad'),
+    VOTEVALUES=[(-2, '-2: Awful'),
+         (-1, '-1: Bad'),
          (0, '0: Neutral'),
          (1, '1: Good'),
          (2, '2: Great'),
+         (None, 'N/A')
          ]
+    
     battlefield = forms.ChoiceField(choices=VOTEVALUES, widget=forms.RadioSelect, required=False)
     final_destination = forms.ChoiceField(label='Final Destination', choices=VOTEVALUES, widget=forms.RadioSelect, required=False)
     pokemon_stadium = forms.ChoiceField(label='Pokémon Stadium 2', choices=VOTEVALUES, widget=forms.RadioSelect, required=False)
@@ -152,12 +154,112 @@ class VoteModelForm(forms.ModelForm):
     yoshi_story = forms.ChoiceField(label="Yoshi's Story", choices=VOTEVALUES, widget=forms.RadioSelect, required=False)
     yoshi_island = forms.ChoiceField(label="Yoshi's Island", choices=VOTEVALUES, widget=forms.RadioSelect, required=False)
     unova = forms.ChoiceField(label='Unova Pokémon League', choices=VOTEVALUES, widget=forms.RadioSelect, required=False)
+    
+
+    # make sure scores are between -2 and 2, and that blank strings convert to None
+    def clean_battlefield(self):
+        battlefield = self.cleaned_data['battlefield']
+        if not battlefield:
+            battlefield = None
+        elif int(battlefield) > 2 or int(battlefield) < -2:
+            raise forms.ValidationError("Only scores between -2 and 2 allowed.")
+        return battlefield
+
+    def clean_final_destination(self):
+        final_destination = self.cleaned_data['final_destination']
+        if not final_destination:
+            final_destination = None
+        elif int(final_destination) > 2 or int(final_destination) < -2:
+            raise forms.ValidationError("Only scores between -2 and 2 allowed.")
+        return final_destination
+    
+    def clean_pokemon_stadium(self):
+        pokemon_stadium = self.cleaned_data['pokemon_stadium']
+        if not pokemon_stadium:
+            pokemon_stadium = None
+        elif int(pokemon_stadium) > 2 or int(pokemon_stadium) < -2:
+            raise forms.ValidationError("Only scores between -2 and 2 allowed.")
+        return pokemon_stadium
+
+    def clean_smashville(self):
+        smashville = self.cleaned_data['smashville']
+        if not smashville:
+            smashville = None
+        elif int(smashville) > 2 or int(smashville) < -2:
+            raise forms.ValidationError("Only scores between -2 and 2 allowed.")
+        return smashville
+    
+    def clean_town(self):
+        town = self.cleaned_data['town']
+        if not town:
+            town = None
+        elif int(town) > 2 or int(town) < -2:
+            raise forms.ValidationError("Only scores between -2 and 2 allowed.")
+        return town
+
+    def clean_lylat(self):
+        lylat = self.cleaned_data['lylat']
+        if not lylat:
+            lylat = None
+        elif int(lylat) > 2 or int(lylat) < -2:
+            raise forms.ValidationError("Only scores between -2 and 2 allowed.")
+        return lylat
+
+    def clean_kalos(self):
+        kalos = self.cleaned_data['kalos']
+        if not kalos:
+            kalos = None
+        elif int(kalos) > 2 or int(kalos) < -2:
+            raise forms.ValidationError("Only scores between -2 and 2 allowed.")
+        return kalos
+
+    def clean_yoshi_story(self):
+        yoshi_story = self.cleaned_data['yoshi_story']
+        if not yoshi_story:
+            yoshi_story = None
+        elif int(yoshi_story) > 2 or int(yoshi_story) < -2:
+            raise forms.ValidationError("Only scores between -2 and 2 allowed.")
+        return yoshi_story
+
+    def clean_yoshi_island(self):
+        yoshi_island = self.cleaned_data['yoshi_island']
+        if not yoshi_island:
+            yoshi_island = None
+        elif int(yoshi_island) > 2 or int(yoshi_island) < -2:
+            raise forms.ValidationError("Only scores between -2 and 2 allowed.")
+        return yoshi_island
+
+    def clean_unova(self):
+        unova = self.cleaned_data['unova']
+        if not unova:
+            unova = None
+        elif int(unova) > 2 or int(unova) < -2:
+            raise forms.ValidationError("Only scores between -2 and 2 allowed.")
+        return unova
 
     class Meta:
         model = Vote
         fields = ['character', 'battlefield', 'final_destination', 'pokemon_stadium', 'smashville',
         'town', 'lylat', 'kalos', 'yoshi_story', 'yoshi_island', 'unova']
+        widgets = {'character': forms.HiddenInput()}
 
-class VoteView(generic.FormView):
+
+class VoteView(generic.CreateView):
     template_name = 'guide/vote.html'
     form_class = VoteModelForm
+    #success_url = reverse('guide:character')
+
+    def get_success_url(self, **kwargs):
+        return reverse('guide:character', args=[self.kwargs['charactername']])
+
+    def get_initial(self, *args, **kwargs):
+        initial = super(VoteView, self).get_initial(**kwargs)
+        initial['character'] = self.kwargs['charactername']
+        return initial
+
+    def get_context_data(self, **kwargs):
+        context = super(VoteView, self).get_context_data(**kwargs)
+        context['character'] = get_object_or_404(Character, pk=self.kwargs['charactername'])
+        return context
+
+    
